ee30c67babf5fd88416c0f0246f7ff008fcb8a89
Greg Brown <grgbrn@gmail.com>
2019-06-26 16:04:27 +0200

    new date control and stub javascript

--- ui/static/js/artists.js
+++ ui/static/js/artists.js
@@ -1,33 +1,84 @@
 const artistDataUrl = "data/artists"
 
-document.addEventListener('DOMContentLoaded', (e) => {
-
-    document.getElementById("daterange").addEventListener('change', e => {
-        console.log("date range was changed:" + e.target.value)
-        populatePage(e.target.value);
-    })
-
-    populatePage('7d')
-})
-
 // since there's only one set of data available for now, cheat
 // and just reverse the order of loaded data on each change
 // to simulate different data being returned from server
 window.populatePageCount = 0;
 
-function populatePage(timePeriod) {
+// simple application state
+window.state = {
+    offset: 0,     // how far back we are from the present
+    mode: "month", // current display mode
+}
+
+document.addEventListener('DOMContentLoaded', (e) => {
+
+    document.getElementById("prevlink").addEventListener('click', e => {
+        window.state.offset += 1
+        updateControls(window.state)
+        populatePage(window.state)
+    })
+
+    document.getElementById("nextlink").addEventListener('click', e => {
+        window.state.offset -= 1
+        updateControls(window.state)
+        populatePage(window.state)
+    })
+
+    document.getElementById("daterange").addEventListener('change', e => {
+
+        console.log("date mode was changed:" + e.target.value)
+        window.state.mode = e.target.value
+
+        /*
+        XXX how to change offset value when switching between modes???
+
+        going from week->month ideally it would display the current month
+        going from month->week it would display first month of the week?
+
+        for now, just reset to 0 which isn't great...
+
+        */
+       window.state.offset = 0
+
+        updateControls(window.state)
+        populatePage(window.state)
+    })
+
+    // call with the default value
+    updateControls(window.state)
+    populatePage(window.state)
+})
+
+function updateControls(state) {
+    console.log(state)
+
+    // set link labels
+    let controlLabel = capitalize(state.mode)
+    document.querySelector("#datebar-prev-label").textContent = controlLabel
+    document.querySelector("#datebar-next-label").textContent = controlLabel
+
+    // disable "next" link if we're at present time
+    if (state.offset == 0) {
+        document.getElementById("nextlink").style.visibility = "hidden";
+    } else {
+        document.getElementById("nextlink").style.visibility = "";
+    }
+}
+
+function populatePage(state) {
 
     // populate artist table with results of api call
     let p1 = fetch(artistDataUrl)
         .then(response => response.json())
         .then(data => {
-            console.log(`got ${data.length} artists from json call`)
+            // console.log(`got ${data.length} artists from json call`)
 
             if (window.populatePageCount % 2 == 1) {
                 data = data.reverse();
             }
 
-            console.log(data);
+            // console.log(data);
 
             var artistGallery = document.querySelector("div.gallery")
             empty(artistGallery)
@@ -64,4 +115,8 @@ function empty(domElt) {
 
 function randElt(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function capitalize(s) {
+    return s && s[0].toUpperCase() + s.slice(1);
 }
