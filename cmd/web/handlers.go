6383770c3586d6c965bca947f83be99b875da2eb
Greg Brown <grgbrn@gmail.com>
2019-07-20 10:27:39 +0200

    separate handlers and http helper fns

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,65 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
-	"runtime/debug"
 	"strconv"
 	"time"
 
 	"bitbucket.org/grgbrn/localfm/pkg/query"
 )
-
-const logStackTraces bool = false
-
-func (app *application) serverError(w http.ResponseWriter, err error) {
-	if logStackTraces {
-		// from "let's go" ch3.04
-		// XXX but this could use some tweaking
-		trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-		//app.err.Println(trace)
-		app.err.Output(2, trace)
-	} else {
-		app.err.Println(err.Error())
-	}
-
-	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-}
-
-// XXX maybe add a clientError also?
-// file:///home/greg/Downloads/lets-go/html/03.04-centralized-error-handling.html
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	templatePath := "./ui/html/" + tmpl + ".html"
-	t, err := template.ParseFiles(templatePath)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-		return
-	}
-
-	err = t.Execute(w, nil)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-	}
-}
-
-func renderJSON(w http.ResponseWriter, status int, payload interface{}) {
-	response, err := json.Marshal(payload)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(status)
-	w.Write([]byte(response))
-}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
