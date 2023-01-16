9be1e80770d5ac18e7baf84d049368ffd359816d
Greg Brown <grgbrn@gmail.com>
2023-01-16 19:03:44 +0100

    Template formatting functions to match old javascript

--- pkg/web/helpers.go
+++ pkg/web/helpers.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"math"
 	"net/http"
 	"path"
 	"runtime/debug"
 	"strings"
+	"time"
 
 	"bitbucket.org/grgbrn/localfm/pkg/util"
 )
@@ -56,7 +58,13 @@ func renderTemplate(w http.ResponseWriter, tmpl string, td interface{}) {
 		prefix + "datebar.partial.tmpl",
 		prefix + "nextbar.partial.tmpl",
 	}
-	ts, err := template.ParseFiles(files...)
+
+	var functions = template.FuncMap{
+		"dateLabel":  dateLabel,
+		"prettyTime": prettyTime,
+	}
+
+	ts, err := template.New(tmpl).Funcs(functions).ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
@@ -80,4 +88,43 @@ func renderJSON(w http.ResponseWriter, status int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(status)
 	w.Write([]byte(response))
+}
+
+// template formatters
+
+// returns "today", "yesterday", or a date string
+func dateLabel(t time.Time) string {
+	now := time.Now()
+	if now.Year() == t.Year() && now.YearDay() == t.YearDay() {
+		return "Today"
+	} else {
+		yesterday := now.AddDate(0, 0, -1)
+		if yesterday.Year() == t.Year() && yesterday.YearDay() == t.YearDay() {
+			return "Yesterday"
+		}
+	}
+	return t.Format("Mon Jan 2 2006")
+}
+
+// returns a relative time if in the last day, otherwise "kitchen" time
+func prettyTime(t time.Time) string {
+	diff := time.Now().Unix() - t.Unix()
+	dayDiff := int(math.Floor(float64(diff) / 86400))
+
+	if dayDiff == 0 {
+		switch {
+		case diff < 60:
+			return "just now"
+		case diff < 120:
+			return "1 minute ago"
+		case diff < 3600:
+			return fmt.Sprintf("%d minutes ago", diff/60)
+		case diff < 7200:
+			return "1 hour ago"
+		case diff < 86400:
+			return fmt.Sprintf("%d hours ago", diff/3600)
+		}
+	}
+
+	return t.Format(time.Kitchen)
 }
